198e679bb374be1ac50690c39525df8cab76ff5b
Shwaam <shwaam@Shwaams-Mac-Mini.local>
2025-09-02 13:58:30 +0200

    Added 'SearchBar' and 'AppTitle' to the main page. Added dynamic 'pressplay; logo.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import pressplay from './pressplay.svg';
 import './App.css';
+import './SearchBar.js';
+import SearchBar from './SearchBar.js';
+import AppTitle from './AppTitle.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={pressplay} className="App-logo" alt="logo" />
+        <div>
+          <AppTitle />
+        </div>
+       <img src={pressplay} className="App-logo" alt="logo" />
+        
       </header>
+
+      <div>
+        <SearchBar />
+      </div>
     </div>
   );
 }
---
+++ src/AppTitle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './AppTitle.css';
+
+export default function AppTitle() {
+  return (
+    <div className="AppTitle">
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+    </div>
+  );
+}
